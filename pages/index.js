31e01971a7a74a8b1e32f4e907876a38f88d63c3
Brian Qiu <brianlqiu5@gmail.com>
2020-08-23 05:03:04 -0700

    landing page design

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Nav from '../components/nav'
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Nav />
+      <div className="py-20">
+        <h1 className="text-gray-800 font-semibold text-5xl text-center">
+          Statstify
+        </h1>
+      </div>
+    </div>
+  )
+}
